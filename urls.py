54ed55e73f5c9141f00763ab27ad6f2fc8505b51
Matthew Jacobi <matt@oppian.com>
2009-02-21 23:57:49 +0000

    create django project file

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^xerobis/', include('xerobis.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
