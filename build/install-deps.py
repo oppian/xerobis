9a2865e6062c6931df6a99da69b22b395e0fb378
Matthew Jacobi <matt@oppian.com>
2009-02-23 01:17:17 +0000

    changing deps install script to install correct version of PIL

--- build/install-deps.py
+++ build/install-deps.py
@@ -5,7 +5,7 @@ os.system('easy_install http://code.enthought.com/enstaller/eggs/rhel/3/Reportla
 os.system('easy_install django-registration')
 os.system('easy_install http://tinyforge.org/frs/download.php/1/trml2pdf.tar.gz')
 os.system('easy_install PyYAML')
-os.system('easy_install --find-links=http://download.zope.org/distribution PILwoTk')
+os.system('easy_install PIL')
 os.system('easy_install http://banjo.assembla.com/spaces/threaded_multihost/documents/dtE4jcUD8r3Abtab7jnrAJ/download/django_threaded_multihost-1.2-py2.5.egg')
 os.system('easy_install elementtree')
 os.system('easy_install docutils')
